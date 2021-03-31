cc5df1ef47b1fddd8a1809d77e8a8dd66d8da002
Rodrigo Nogueira <you@example.com>
2021-03-31 08:53:30 -0300

    React-zzaria - Marcação inicial do app de

--- sistema-cadastro/src/pages/main/main.js
+++ sistema-cadastro/src/pages/main/main.js
@@ -1,22 +1,36 @@
 import React, { Suspense } from 'react'
+import styled from 'styled-components'
 import { Switch, Route } from 'react-router-dom'
+import { Drawer as MaterialDrawer } from '@material-ui/core'
 
 const Main = () => (
     <>
+    <Drawer variant='permanent'>
+        drawer
+    </Drawer>
+    <Content>
         <Suspense fallback='Loading...'>
             <Switch>
 
                 <Route>
                     <h1>main</h1>
                 </Route>
-
-                {/* <Route path={routes.HOME} exact children={<ChoosePizzaSize />} 
-                    // React.createElement(ChoosePizzaSize, null, children)
-                    //Isto é executar um component react quanto se usa JSX
-                /> */}
             </Switch>
-        </Suspense>
+        </Suspense>        
+    </Content>
     </>
 )
+
+const Drawer = styled(MaterialDrawer)`
+    && {
+        .MuiPaper-root {
+            width: ${({theme}) => theme.extend.drawerWidth}px;
+        }
+    }
+`
+const Content = styled.main`
+    margin-left: ${({theme}) => theme.extend.drawerWidth}px;
+    padding: ${({ theme }) => theme.spacing(3)}
+`
 
 export default Main
--- sistema-cadastro/src/root.js
+++ sistema-cadastro/src/root.js
@@ -13,6 +13,9 @@ import App from './app'
 const theme = createMuiTheme({
   typegraphy: {
     useNextVariants: true
+  },
+  extend: {
+    drawerWidth: 280
   }
 })
 
