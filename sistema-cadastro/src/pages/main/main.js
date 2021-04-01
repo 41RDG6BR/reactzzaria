05c90e6584199efea965dba80863fe274808ae27
Rodrigo Nogueira <you@example.com>
2021-04-01 07:58:00 -0300

    Selecionar item de menu conforme página atual

--- sistema-cadastro/src/pages/main/main.js
+++ sistema-cadastro/src/pages/main/main.js
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from 'react'
 import styled from 'styled-components'
-import { Link, Switch, Route } from 'react-router-dom'
+import { Link, Switch, Route, useLocation } from 'react-router-dom'
 import { 
     Divider,
     Drawer as MaterialDrawer,
@@ -15,50 +15,57 @@ import {
  const PizzasFlavours = lazy(()=> import('../pizzas-flavours'))
  const PizzasSizes = lazy(()=> import('../pizzas-sizes'))
 
-const Main = () => (
-    <>
-    <Drawer variant='permanent'>
-        <DrawerContent>
-            <Typography variant='h4'>
-                Pizzaria
-            </Typography>
- 
-            <Typography>
-                sistema cadastro
-            </Typography>
-        </DrawerContent>
+const Main = () => {
+
+    const { pathname } = useLocation();
+
+    return(
+        <>
+            <Drawer variant='permanent'>
+                <DrawerContent>
+                    <Typography variant='h4'>
+                        Pizzaria
+                    </Typography>
         
-        <Divider />
+                    <Typography>
+                        sistema cadastro
+                    </Typography>
+                </DrawerContent>
+                
+                <Divider />
 
-    <List>
-        {menuItems.map(item => (
-            <ListItem 
-                key ={item.label} 
-                button
-                component={Link}
-                to={item.link}
-            >
-                <ListItemText>
-                    {item.label}
-                </ListItemText>
-            </ListItem>
-        ))}
-    </List>
-
-    </Drawer>
-    <Content>
-        <Suspense fallback='Loading...'>
-            <Switch>
-                {menuItems.map(item => (            
-                    <Route key={item.link} path={item.link} exact={item.exact}>
-                        <item.component />
-                    </Route>
+            <List>
+                {menuItems.map(item => (
+                    <ListItem 
+                    key ={item.label} 
+                    button
+                    selected={pathname === item.link}
+                    component={Link}
+                    to={item.link}
+                    >
+                        <ListItemText>
+                            {item.label}
+                        </ListItemText>
+                    </ListItem>
                 ))}
-            </Switch>
-        </Suspense>        
-    </Content>
-    </>
-)
+            </List>
+
+            </Drawer>
+            <Content>
+                <Suspense fallback='Loading...'>
+                    <Switch>
+                        {menuItems.map(item => (            
+                            <Route key={item.link} path={item.link} exact={item.exact}>
+                                <item.component />
+                            </Route>
+                        ))}
+                    </Switch>
+                </Suspense>        
+            </Content>
+        </>
+    )
+}
+
 
 const menuItems = [
     {
