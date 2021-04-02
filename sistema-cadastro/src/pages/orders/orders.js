0b74995e95596432b07bee8abd24697abedc4c9c
Rodrigo Nogueira <you@example.com>
2021-04-02 07:36:16 -0300

    Listagem de pedidos

--- sistema-cadastro/src/pages/orders/orders.js
+++ sistema-cadastro/src/pages/orders/orders.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
+import styled from 'styled-components'
+import { 
+    Paper,
+    Table,
+    TableHead,
+    TableRow,
+    TableContainer,
+    TableCell,
+    TableBody,
+    Typography
+ } from '@material-ui/core'
 
-function Orders () {
-    return <h1>Pedidos</h1>
-} 
+const Orders = () => {
+    return(
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            <Typography>
+                            Informações do pedido
+                            </Typography>
+                        </TableCell>
+                    </TableRow>
+                </TableHead>    
+
+                <TableBody>
+                    <TableRow>
+                        <TableCell>
+                            <div>
+                                <Typography>
+                                    Horário do pedido: 4:20h
+                                </Typography>
+                            </div>
+                            <div>
+                                <Typography>
+                                    Pedido:
+                                </Typography>
+                                <ul>
+                                    <li>
+                                        <Typography>
+                                            1 pizza media de {' '}
+                                            Frango com Catupiry e Calabresa
+                                        </Typography>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <Typography>
+                                    Endereço do pedido
+                                </Typography>
+
+                                <Typography>
+                                    Rua Tal, n°, {' '}
+                                    ap 10 <br />
+                                    Bairro: Boa vista - CEP: 82560440 <br />
+                                    Curitiba/PR
+                                </Typography>
+                            </div>
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
 
 export default Orders
